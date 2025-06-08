363c06dc95300a046cde04453758c29d7be443cb
ArthurZheng <869705086@qq.com>
2025-06-08 19:23:50 +0800

    Fixed serious errors in esp32s3 control board. Adjusted the pin list and function blocks.

--- src/devices/arduinoEsp32S3/arduinoEsp32S3.js
+++ src/devices/arduinoEsp32S3/arduinoEsp32S3.js
@@ -13,6 +13,8 @@ const CommonPeripheral = require('../common/common-peripheral');
 const PNPID_LIST = [
     // CH340
     'USB\\VID_1A86&PID_7523',
+    // CH343
+    'USB\\VID_1A86&PID_55D3',
     // CH9102
     'USB\\VID_1A86&PID_55D4',
     // CP2102
@@ -80,42 +82,23 @@ const Pins = {
     IO17: '17',
     IO18: '18',
     IO19: '19',
+    IO20: '20',
     IO21: '21',
-    IO22: '22',
-    IO23: '23',
-    IO25: '25',
-    IO26: '26',
-    IO27: '27',
-    IO32: '32',
-    IO33: '33',
-    IO34: '34',
     IO35: '35',
     IO36: '36',
-    IO39: '39'
+    IO37: '37',
+    IO38: '38',
+    IO39: '39',
+    IO40: '40',
+    IO41: '41',
+    IO42: '42',
+    IO43: '43',
+    IO44: '44'
 };
 
 const Level = {
     High: 'HIGH',
     Low: 'LOW'
-};
-
-const Channels = {
-    CH0: '0',
-    CH1: '1',
-    CH2: '2',
-    CH3: '3',
-    CH4: '4',
-    CH5: '5',
-    CH6: '6',
-    CH7: '7',
-    CH8: '8',
-    CH9: '9',
-    CH10: '10',
-    CH11: '11',
-    CH12: '12',
-    CH13: '13',
-    CH14: '14',
-    CH15: '15'
 };
 
 const SerialNo = {
@@ -212,31 +195,30 @@ class OpenBlockArduinoEsp32S3Device {
                 text: 'IO5',
                 value: Pins.IO5
             },
-            // Pins 6 to 11 are used by the ESP32 Flash, not recommended for general use.
-            // {
-            //     text: 'IO6',
-            //     value: Pins.IO6
-            // },
-            // {
-            //     text: 'IO7',
-            //     value: Pins.IO7
-            // },
-            // {
-            //     text: 'IO8',
-            //     value: Pins.IO8
-            // },
-            // {
-            //     text: 'IO9',
-            //     value: Pins.IO9
-            // },
-            // {
-            //     text: 'IO10',
-            //     value: Pins.IO10
-            // },
-            // {
-            //     text: 'IO11',
-            //     value: Pins.IO11
-            // },
+            {
+                text: 'IO6',
+                value: Pins.IO6
+            },
+            {
+                text: 'IO7',
+                value: Pins.IO7
+            },
+            {
+                text: 'IO8',
+                value: Pins.IO8
+            },
+            {
+                text: 'IO9',
+                value: Pins.IO9
+            },
+            {
+                text: 'IO10',
+                value: Pins.IO10
+            },
+            {
+                text: 'IO11',
+                value: Pins.IO11
+            },
             {
                 text: 'IO12',
                 value: Pins.IO12
@@ -270,170 +252,53 @@ class OpenBlockArduinoEsp32S3Device {
                 value: Pins.IO19
             },
             {
+                text: 'IO20',
+                value: Pins.IO20
+            },
+            {
                 text: 'IO21',
                 value: Pins.IO21
             },
+            // IO35-37 are used by octal flash, not recommended for general use.
+            // {
+            //     text: 'IO35',
+            //     value: Pins.IO35
+            // },
+            // {
+            //     text: 'IO36',
+            //     value: Pins.IO36
+            // },
+            // {
+            //     text: 'IO37',
+            //     value: Pins.IO37
+            // },
             {
-                text: 'IO22',
-                value: Pins.IO22
-            },
-            {
-                text: 'IO23',
-                value: Pins.IO23
-            },
-            {
-                text: 'IO25',
-                value: Pins.IO25
-            },
-            {
-                text: 'IO26',
-                value: Pins.IO26
-            },
-            {
-                text: 'IO27',
-                value: Pins.IO27
-            },
-            {
-                text: 'IO32',
-                value: Pins.IO32
-            },
-            {
-                text: 'IO33',
-                value: Pins.IO33
-            },
-            {
-                text: 'IO34',
-                value: Pins.IO34
-            },
-            {
-                text: 'IO35',
-                value: Pins.IO35
-            },
-            {
-                text: 'IO36',
-                value: Pins.IO36
+                text: 'IO38',
+                value: Pins.IO38
             },
             {
                 text: 'IO39',
                 value: Pins.IO39
-            }
-        ];
-    }
-
-    get OUT_PINS_MENU () {
-        return [
-            {
-                text: 'IO0',
-                value: Pins.IO0
             },
             {
-                text: 'IO1',
-                value: Pins.IO1
+                text: 'IO40',
+                value: Pins.IO40
             },
             {
-                text: 'IO2',
-                value: Pins.IO2
+                text: 'IO41',
+                value: Pins.IO41
             },
             {
-                text: 'IO3',
-                value: Pins.IO3
+                text: 'IO42',
+                value: Pins.IO42
             },
             {
-                text: 'IO4',
-                value: Pins.IO4
+                text: 'IO43',
+                value: Pins.IO43
             },
             {
-                text: 'IO5',
-                value: Pins.IO5
-            },
-            // Pins 6 to 11 are used by the ESP32-S3 Flash, not recommended for general use.
-            // {
-            //     text: 'IO6',
-            //     value: Pins.IO6
-            // },
-            // {
-            //     text: 'IO7',
-            //     value: Pins.IO7
-            // },
-            // {
-            //     text: 'IO8',
-            //     value: Pins.IO8
-            // },
-            // {
-            //     text: 'IO9',
-            //     value: Pins.IO9
-            // },
-            // {
-            //     text: 'IO10',
-            //     value: Pins.IO10
-            // },
-            // {
-            //     text: 'IO11',
-            //     value: Pins.IO11
-            // },
-            {
-                text: 'IO12',
-                value: Pins.IO12
-            },
-            {
-                text: 'IO13',
-                value: Pins.IO13
-            },
-            {
-                text: 'IO14',
-                value: Pins.IO14
-            },
-            {
-                text: 'IO15',
-                value: Pins.IO15
-            },
-            {
-                text: 'IO16',
-                value: Pins.IO16
-            },
-            {
-                text: 'IO17',
-                value: Pins.IO17
-            },
-            {
-                text: 'IO18',
-                value: Pins.IO18
-            },
-            {
-                text: 'IO19',
-                value: Pins.IO19
-            },
-            {
-                text: 'IO21',
-                value: Pins.IO21
-            },
-            {
-                text: 'IO22',
-                value: Pins.IO22
-            },
-            {
-                text: 'IO23',
-                value: Pins.IO23
-            },
-            {
-                text: 'IO25',
-                value: Pins.IO25
-            },
-            {
-                text: 'IO26',
-                value: Pins.IO26
-            },
-            {
-                text: 'IO27',
-                value: Pins.IO27
-            },
-            {
-                text: 'IO32',
-                value: Pins.IO32
-            },
-            {
-                text: 'IO33',
-                value: Pins.IO33
+                text: 'IO44',
+                value: Pins.IO44
             }
         ];
     }
@@ -478,16 +343,48 @@ class OpenBlockArduinoEsp32S3Device {
     get ANALOG_PINS_MENU () {
         return [
             {
-                text: 'IO0',
-                value: Pins.IO0
+                text: 'IO1',
+                value: Pins.IO1
             },
             {
                 text: 'IO2',
                 value: Pins.IO2
             },
             {
+                text: 'IO3',
+                value: Pins.IO3
+            },
+            {
                 text: 'IO4',
                 value: Pins.IO4
+            },
+            {
+                text: 'IO5',
+                value: Pins.IO5
+            },
+            {
+                text: 'IO6',
+                value: Pins.IO6
+            },
+            {
+                text: 'IO7',
+                value: Pins.IO7
+            },
+            {
+                text: 'IO8',
+                value: Pins.IO8
+            },
+            {
+                text: 'IO9',
+                value: Pins.IO9
+            },
+            {
+                text: 'IO10',
+                value: Pins.IO10
+            },
+            {
+                text: 'IO11',
+                value: Pins.IO11
             },
             {
                 text: 'IO12',
@@ -506,40 +403,24 @@ class OpenBlockArduinoEsp32S3Device {
                 value: Pins.IO15
             },
             {
-                text: 'IO25',
-                value: Pins.IO25
+                text: 'IO16',
+                value: Pins.IO16
             },
             {
-                text: 'IO26',
-                value: Pins.IO26
+                text: 'IO17',
+                value: Pins.IO17
             },
             {
-                text: 'IO27',
-                value: Pins.IO27
+                text: 'IO18',
+                value: Pins.IO18
             },
             {
-                text: 'IO32',
-                value: Pins.IO32
+                text: 'IO19',
+                value: Pins.IO19
             },
             {
-                text: 'IO33',
-                value: Pins.IO33
-            },
-            {
-                text: 'IO34',
-                value: Pins.IO34
-            },
-            {
-                text: 'IO35',
-                value: Pins.IO35
-            },
-            {
-                text: 'IO36',
-                value: Pins.IO36
-            },
-            {
-                text: 'IO39',
-                value: Pins.IO39
+                text: 'IO20',
+                value: Pins.IO20
             }
         ];
     }
@@ -565,101 +446,51 @@ class OpenBlockArduinoEsp32S3Device {
         ];
     }
 
-    get LEDC_CHANNELS_MENU () {
-        return [
-            {
-                text: 'CH0 (LT0)',
-                value: Channels.CH0
-            },
-            {
-                text: 'CH1 (LT0)',
-                value: Channels.CH1
-            },
-            {
-                text: 'CH2 (LT1)',
-                value: Channels.CH2
-            },
-            {
-                text: 'CH3 (LT1)',
-                value: Channels.CH3
-            },
-            {
-                text: 'CH4 (LT2)',
-                value: Channels.CH4
-            },
-            {
-                text: 'CH5 (LT2)',
-                value: Channels.CH5
-            },
-            {
-                text: 'CH6 (LT3)',
-                value: Channels.CH6
-            },
-            {
-                text: 'CH7 (LT3)',
-                value: Channels.CH7
-            },
-            {
-                text: 'CH8 (HT0)',
-                value: Channels.CH8
-            },
-            {
-                text: 'CH9 (HT0)',
-                value: Channels.CH9
-            },
-            {
-                text: 'CH10 (HT1)',
-                value: Channels.CH10
-            },
-            {
-                text: 'CH11 (HT1)',
-                value: Channels.CH11
-            },
-            {
-                text: 'CH12 (HT2)',
-                value: Channels.CH12
-            },
-            {
-                text: 'CH13 (HT2)',
-                value: Channels.CH13
-            },
-            {
-                text: 'CH14 (HT3)',
-                value: Channels.CH14
-            },
-            {
-                text: 'CH15 (HT3)',
-                value: Channels.CH15
-            }
-        ];
-    }
-
-    get DAC_PINS_MENU () {
-        return [
-            {
-                text: 'IO25',
-                value: Pins.IO25
-            },
-            {
-                text: 'IO26',
-                value: Pins.IO26
-            }
-        ];
-    }
-
     get TOUCH_PINS_MENU () {
         return [
             {
-                text: 'IO0',
-                value: Pins.IO0
+                text: 'IO1',
+                value: Pins.IO1
             },
             {
                 text: 'IO2',
                 value: Pins.IO2
             },
             {
+                text: 'IO3',
+                value: Pins.IO3
+            },
+            {
                 text: 'IO4',
                 value: Pins.IO4
+            },
+            {
+                text: 'IO5',
+                value: Pins.IO5
+            },
+            {
+                text: 'IO6',
+                value: Pins.IO6
+            },
+            {
+                text: 'IO7',
+                value: Pins.IO7
+            },
+            {
+                text: 'IO8',
+                value: Pins.IO8
+            },
+            {
+                text: 'IO9',
+                value: Pins.IO9
+            },
+            {
+                text: 'IO10',
+                value: Pins.IO10
+            },
+            {
+                text: 'IO11',
+                value: Pins.IO11
             },
             {
                 text: 'IO12',
@@ -676,18 +507,6 @@ class OpenBlockArduinoEsp32S3Device {
             {
                 text: 'IO15',
                 value: Pins.IO15
-            },
-            {
-                text: 'IO27',
-                value: Pins.IO27
-            },
-            {
-                text: 'IO32',
-                value: Pins.IO32
-            },
-            {
-                text: 'IO33',
-                value: Pins.IO33
             }
         ];
     }
@@ -743,11 +562,10 @@ class OpenBlockArduinoEsp32S3Device {
                 text: '0',
                 value: SerialNo.Serial0
             },
-            // Usually IO9/10 is reserved for flash chip.
-            // {
-            //     text: '1',
-            //     value: SerialNo.Serial1
-            // },
+            {
+                text: '1',
+                value: SerialNo.Serial1
+            },
             {
                 text: '2',
                 value: SerialNo.Serial2
@@ -903,7 +721,7 @@ class OpenBlockArduinoEsp32S3Device {
                         arguments: {
                             PIN: {
                                 type: ArgumentType.STRING,
-                                menu: 'outPins',
+                                menu: 'pins',
                                 defaultValue: Pins.IO2
                             },
                             LEVEL: {
@@ -916,46 +734,20 @@ class OpenBlockArduinoEsp32S3Device {
                     {
                         opcode: 'esp32SetPwmOutput',
                         text: formatMessage({
-                            id: 'arduinoEsp32S3.pins.esp32SetPwmOutput',
-                            default: 'set pwm pin [PIN] use channel [CH] out [OUT]',
-                            description: 'arduinoEsp32S3 set pwm pin out'
+                            id: 'arduinoEsp32.pins.esp32SetPwmOutput',
+                            default: 'set pwm pin [PIN] out [OUT]',
+                            description: 'arduinoEsp32 set pwm pin out'
                         }),
                         blockType: BlockType.COMMAND,
                         arguments: {
                             PIN: {
                                 type: ArgumentType.STRING,
-                                menu: 'outPins',
+                                menu: 'pins',
                                 defaultValue: Pins.IO2
                             },
                             OUT: {
                                 type: ArgumentType.UINT8_NUMBER,
                                 defaultValue: '255'
-                            },
-                            CH: {
-                                type: ArgumentType.NUMBER,
-                                menu: 'ledcChannels',
-                                defaultValue: Channels.CH0
-                            }
-                        }
-                    },
-                    {
-
-                        opcode: 'esp32SetDACOutput',
-                        text: formatMessage({
-                            id: 'arduinoEsp32S3.pins.esp32SetDACOutput',
-                            default: 'set dac pin [PIN] out [OUT]',
-                            description: 'arduinoEsp32S3 set dac pin out'
-                        }),
-                        blockType: BlockType.COMMAND,
-                        arguments: {
-                            PIN: {
-                                type: ArgumentType.STRING,
-                                menu: 'dacPins',
-                                defaultValue: Pins.IO25
-                            },
-                            OUT: {
-                                type: ArgumentType.UINT8_NUMBER,
-                                defaultValue: '0'
                             }
                         }
                     },
@@ -1013,25 +805,20 @@ class OpenBlockArduinoEsp32S3Device {
 
                         opcode: 'esp32SetServoOutput',
                         text: formatMessage({
-                            id: 'arduinoEsp32S3.pins.setServoOutput',
-                            default: 'set servo pin [PIN] use channel [CH] out [OUT]',
-                            description: 'arduinoEsp32S3 set servo pin out'
+                            id: 'arduinoEsp32.pins.esp32SetServoOutput',
+                            default: 'set servo pin [PIN] out [OUT]',
+                            description: 'arduinoEsp32 set servo pin out'
                         }),
                         blockType: BlockType.COMMAND,
                         arguments: {
                             PIN: {
                                 type: ArgumentType.STRING,
-                                menu: 'outPins',
+                                menu: 'pins',
                                 defaultValue: Pins.IO2
                             },
                             OUT: {
                                 type: ArgumentType.HALF_ANGLE,
                                 defaultValue: '90'
-                            },
-                            CH: {
-                                type: ArgumentType.NUMBER,
-                                menu: 'ledcChannels',
-                                defaultValue: Channels.CH0
                             }
                         }
                     },
@@ -1082,9 +869,6 @@ class OpenBlockArduinoEsp32S3Device {
                     pins: {
                         items: this.PINS_MENU
                     },
-                    outPins: {
-                        items: this.OUT_PINS_MENU
-                    },
                     mode: {
                         items: this.MODE_MENU
                     },
@@ -1094,12 +878,6 @@ class OpenBlockArduinoEsp32S3Device {
                     level: {
                         acceptReporters: true,
                         items: this.LEVEL_MENU
-                    },
-                    ledcChannels: {
-                        items: this.LEDC_CHANNELS_MENU
-                    },
-                    dacPins: {
-                        items: this.DAC_PINS_MENU
                     },
                     touchPins: {
                         items: this.TOUCH_PINS_MENU
